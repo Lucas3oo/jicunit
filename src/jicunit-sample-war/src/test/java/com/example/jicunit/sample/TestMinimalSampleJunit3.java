90d876414c25bcba09c595867feabdfbbdfc7164
lucas3oo <lucas+github@solrike.se>
2014-08-08 15:45:15 +0200

    remove extra files

--- src/jicunit-sample-war/src/test/java/com/example/jicunit/sample/TestMinimalSampleJunit3.java
+++
@@ -1,8 +0,0 @@
-package com.example.jicunit.sample;
-
-import org.jicunit.framework.JicUnitTestCase;
-public class TestMinimalSampleJunit3 extends JicUnitTestCase {
-  public void testDoSomething() throws Exception {
-    System.out.println("hello world JUnit3" + " using thread " + Thread.currentThread().getName());
-  }
-}
